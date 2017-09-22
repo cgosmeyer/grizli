cfe2a5c8857b1065257d9adb10073df8c0c846d4
Catherine (Katie) Gosmeyer <cgosmeyer@stsci.edu>
2017-09-22 16:55:51 -0400

    Committing config.py that includes the default paths to RAW, outputs, Persistence, etc.

---
+++ grizli/config.py
@@ -0,0 +1,10 @@
+"""
+Set default paths, etc.
+All grizli modules should read from this.
+No paths anywhere else should be coded.
+"""
+
+PATH_RAW = '../RAW'
+PATH_OUTPUTS  = ''
+PATH_PERSISTENCE = '../Persistence'
+#PATH_LOGS = '../logs'
