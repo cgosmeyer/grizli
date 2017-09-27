6621caa5723761100387d10b4427543f170a5b79
Catherine (Katie) Gosmeyer <cgosmeyer@stsci.edu>
2017-09-27 17:35:51 -0400

    Cleaned up config.py.

--- grizli/config.py
+++ grizli/config.py
@@ -2,9 +2,18 @@
 Set default paths, etc.
 All grizli modules should read from this.
 No paths anywhere else should be coded.
+
+Use
+---
+	# Example within grizli 
+	from . import config
+	path_raw = config.PATH_RAW
+
+	# Example if using in your own pipeline
+	from grizli import config
+	path_raw = config.PATH_RAW
 """
 
 PATH_RAW = '../RAW'
-PATH_OUTPUTS  = ''
 PATH_PERSISTENCE = '../Persistence'
-#PATH_LOGS = '../logs'
+PATH_LOGS = '../logs'
