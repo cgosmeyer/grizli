c15d6db17232bc11a0558e1a79ad579244469449
Catherine (Katie) Gosmeyer <cgosmeyer@stsci.edu>
2017-10-02 19:06:37 -0400

    First take at wrapping all the grizli setup in setup.py and post_install.py. Currently it assumes astrocodna distribution with python 3.

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 #from distutils.core import setup
 #from distutils.extension import Extension
+
+# ASSUMPTIONS:
+# 1. in anaconda (preferably with astroconda distribution), and therefore bash
+# 2. python 3
+
 from setuptools import setup
 from setuptools.extension import Extension
 
-import subprocess
-
 import os
 import numpy
+import pip
+import subprocess
 
 try:
     from Cython.Build import cythonize
@@ -51,6 +56,31 @@ print('Git version: {0}'.format(version))
 if USE_CYTHON:
     extensions = cythonize(extensions)
 
+# Pip install and git clone dependancies. 
+# Note that all git installs will be at same level as "grizli"
+pip_packages = ['peakutils', 'scikit-learn', 'astroquery', 'shapely', 'reproject']
+# photutils, pysynphot, stwcs, drizzlepac -- why weren't these installed with astroconda?
+git_packages = {'lacosmicx':'https://github.com/cmccully/lacosmicx.git', 
+                'sewpy':'https://github.com/gbrammer/sewpy.git'}
+
+def pip_install(package):
+    pip.main(['install', package])
+
+def git_clone(package, url):
+    os.chdir('../') # cd to location just outside of grizli location
+    subprocess.call(['git', 'clone', url])
+    os.chdir(package)
+    subprocess.call(['python', 'setup.py', 'install'])
+
+def install_dependencies():
+    for package in pip_packages:
+        pip_install(package)
+    #for package, url in zip(git_packages.keys(), git_packages.values()):
+    #    git_clone(package, url)
+    #os.chdir('../grizli')
+
+install_dependencies()
+
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
@@ -68,8 +98,13 @@ setup(
     url = "https://github.com/gbrammer/grizli",
     download_url = "https://github.com/gbrammer/grizli/tarball/0.2.1",
     packages=['grizli', 'grizli/utils_c', 'grizli/tests'],
-    # requires=['numpy', 'scipy', 'astropy', 'drizzlepac', 'stwcs'],
+    # the install_requires isn't working for me, but *should* work for normal
+    # people who don't destroy their conda installations...
+    #install_requires=['drizzlepac', 'stwcs', 'photutils', 'pysynphot', 'peakutils', 
+    #    'scikit-learn', 'astroquery', 'shapely', 'reproject'],
     # long_description=read('README.rst'),
+    dependency_links=['https://github.com/cmccully/lacosmicx.git', 
+    'https://github.com/gbrammer/sewpy.git'],
     classifiers=[
         "Development Status :: 1 - Planning",
         'Intended Audience :: Science/Research',
@@ -79,3 +114,4 @@ setup(
     package_data={'grizli': ['data/*', 'data/templates/*', 'data/templates/stars/*', 'data/templates/fsps/*']},
     # scripts=['grizli/scripts/flt_info.sh'],
 )
+
